9ea6252205c0d481a0697bd3ed9bbb5cad723d10
HyeongJinK <wkzkfmxk23@gmail.com>
2021-05-03 01:46:39 +0900

    feat: 자동차 이름 입력 구현 자동차 이름을 입력받아 ','로 구분하여 문자열 배열을 리턴한다. 자동차 이름이 5글자를 넘어가면 예외를 리턴한다.

--- src/main/java/khj/input/RacingGameInput.java
+++ src/main/java/khj/input/RacingGameInput.java
@@ -2,8 +2,28 @@ package khj.input;
 
 import khj.exception.CarNameLengthOverException;
 
+import java.util.Scanner;
+
 public class RacingGameInput {
+    private final int CAR_NAME_LENGTH_MAX = 5;
+    private final String DELIMITER = ",";
+
     public String[] inputCarNames() throws CarNameLengthOverException {
-        return null;
+        Scanner scanner = new Scanner(System.in);
+        String carNames = scanner.nextLine();
+
+        String[] carNameArray = carNames.split(DELIMITER);
+
+        for (String carName: carNameArray) {
+            carNameLengthCheck(carName);
+        }
+
+        return carNameArray;
+    }
+
+    private void carNameLengthCheck(String carName) {
+        if (carName.length() > CAR_NAME_LENGTH_MAX) {
+            throw new CarNameLengthOverException();
+        }
     }
 }
